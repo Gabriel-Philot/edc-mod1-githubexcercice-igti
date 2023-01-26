7e73ed7fd6d8f60cc42dd2e61cada74241ed8617
Gabriel-Philot <bilphilot@gmail.com>
2023-01-26 20:01:35 -0300

    test deploy and back addition and tf infra refacturing

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # edc-mod1-githubexcercice-igti
+
+## First Deploy pipeline in Github
+
+#### Terraform
+
+#### Aws Services
--- infra_terraform/s3.tf
+++ infra_terraform/s3.tf
@@ -36,12 +36,3 @@ resource "aws_s3_bucket_public_access_block" "datalake_public_access_block" {
   restrict_public_buckets = true
 }
 
-
-
-resource "aws_s3_object" "codigo_spark" {
-  bucket = aws_s3_bucket.datalake.id #Referêrncia do datalake criado
-  key    = "emr-code/pyspark/job_spark_fro_tf.py"
-  source = "../job_spark.py"          # Arquivo com o job spark que vai
-  etag   = filemd5("../job_spark.py") # Define qual é o objeto de parâmetro
-}
-
