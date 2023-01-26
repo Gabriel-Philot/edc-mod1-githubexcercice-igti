a2cc4364ca31dab54cf83b63ee73c01ff9411c3a
Gabriel-Philot <bilphilot@gmail.com>
2023-01-26 20:38:42 -0300

    duplicate s3 file fixing

--- infra_terraform/s3_files.tf
+++ infra_terraform/s3_files.tf
@@ -1,7 +1,7 @@
 
 
 # File spark job for emr
-resource "aws_s3_object" "codigo_spark" {
+resource "aws_s3_object" "codigo_spark_emr" {
   bucket = aws_s3_bucket.datalake.id #Referêrncia do datalake criado
   key    = "emr-code/pyspark/emr_job_spark_fro_tf.py"
   source = "../job_spark.py"          # Arquivo com o job spark que vai
@@ -9,7 +9,7 @@ resource "aws_s3_object" "codigo_spark" {
 }
 
 # File spark job for glue
-resource "aws_s3_object" "codigo_spark" {
+resource "aws_s3_object" "codigo_spark_glue" {
   bucket = aws_s3_bucket.datalake.id #Referêrncia do datalake criado
   key    = "glue_job-code/pyspark/gluejob_spark_fro_tf.py"
   source = "../job_spark_glue.py"          # Arquivo com o job spark que vai
